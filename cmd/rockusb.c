6336d2324985831ed766031f91d410d0e587dbc8
Peter Geis <pgwipeout@gmail.com>
2022-02-16 00:39:16 +0000

    rockusb: don't block reads above 32MB

--- cmd/rockusb.c
+++ cmd/rockusb.c
@@ -24,12 +24,7 @@ static int rkusb_read_sector(struct ums *ums_dev,
 	struct blk_desc *block_dev = &ums_dev->block_dev;
 	lbaint_t blkstart = start + ums_dev->start_sector;
 
-	if ((blkstart + blkcnt) > RKUSB_READ_LIMIT_ADDR) {
-		memset(buf, 0xcc, blkcnt * SECTOR_SIZE);
-		return blkcnt;
-	} else {
-		return blk_dread(block_dev, blkstart, blkcnt, buf);
-	}
+	return blk_dread(block_dev, blkstart, blkcnt, buf);
 }
 
 static int rkusb_write_sector(struct ums *ums_dev,
